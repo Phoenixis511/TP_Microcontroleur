9f59041e07db21ccd088bfd5c47c00043cb65c92
JDelayat <joandelayat64500@gmail.com>
2025-11-13 11:14:50 +0100

    Add files via upload

---
+++ main.s
@@ -0,0 +1,19 @@
+
+CLEAN SUCCESSFUL (total time: 51ms)
+"C:\Program Files\Microchip\xc8\v3.10\pic-as\bin\pic-as.exe" -mcpu=PIC18F25K40 -c \
+-o build/default/production/_ext/255251204/main.o \
+"../../../../Users/Joan DELAYAT/Desktop/main.s" \
+  -mdfp="C:/Program Files/Microchip/MPLABX/v6.25/packs/Microchip/PIC18F-K_DFP/1.14.301/xc8"  -misa=std -msummary=+mem,-psect,-class,-hex,-file,-sha1,-sha256,-xml,-xmlfull -fmax-errors=20 -mwarn=0 -xassembler-with-cpp
+../../../../Users/Joan DELAYAT/Desktop/main.s:32:: warning: (1522) RAM access bit operand not specified, assuming access-bank
+../../../../Users/Joan DELAYAT/Desktop/main.s:33:: warning: (1522) RAM access bit operand not specified, assuming access-bank
+::: warning: (1311) missing configuration setting for config word 0x300001; using default
+::: warning: (1311) missing configuration setting for config word 0x300002; using default
+::: warning: (1311) missing configuration setting for config word 0x300003; using default
+::: warning: (1311) missing configuration setting for config word 0x300005; using default
+::: warning: (1311) missing configuration setting for config word 0x300006; using default
+::: warning: (1311) missing configuration setting for config word 0x300007; using default
+::: warning: (1311) missing configuration setting for config word 0x300008; using default
+::: warning: (1311) missing configuration setting for config word 0x30000A; using default
+::: warning: (1311) missing configuration setting for config word 0x30000B; using default
+
+BUILD SUCCESSFUL (total time: 459ms)
